b6357817900347fe4b07752142eb2875a041a65f
kgrabalski <kgrabalski@poczta.onet.pl>
2014-03-28 16:07:18 +0100

    20140328

---
+++ FoodSearch.Presentation.Mobile.Common/GetResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FoodSearch.Presentation.Mobile.Common
+{
+	public class GetResponse
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string BirthDate { get; set; }
+		public string Group { get; set; }
+	}
+}
+
