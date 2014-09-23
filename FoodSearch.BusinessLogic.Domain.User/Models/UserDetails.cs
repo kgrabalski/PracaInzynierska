b85b7a2be17b9a9dedd0cb3844c6b0866a2a482c
Krzysztof Grabalski <kgrabalski@poczta.onet.pl>
2014-09-23 13:38:24 +0200

    missing userdetails

---
+++ FoodSearch.BusinessLogic.Domain.User/Models/UserDetails.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FoodSearch.BusinessLogic.Domain.User.Models
+{
+    public class UserDetails
+    {
+        public Guid UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
