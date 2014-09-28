faef98eacbe04c2fb46a62599f3186959af069b1
Krzysztof Grabalski <kgrabalski@poczta.onet.pl>
2014-09-28 13:19:25 +0200

    usuniecie projektow mobilnych

--- FoodSearch.Service.Contracts/Response/Street.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace FoodSearch.Service.Contracts.Response
-{
-    public class Street
-    {
-        public int StreetId { get; set; }
-        public string Name { get; set; }
-    }
-}
--- FoodSearch.Service.Contracts/Response/StreetNumber.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace FoodSearch.Service.Contracts.Response
-{
-    public class StreetNumber
-    {
-        public int AddressId { get; set; }
-        public string Number { get; set; }
-    }
-}
